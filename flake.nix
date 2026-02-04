5cebf70a4545469528c36fbe687c7041f26ee387
tar-valon <130228090+tar-valon@users.noreply.github.com>
2026-02-04 23:47:56 +0100

    First Commit

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Rust shell for chip-8 emulator project";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: let
+    pkgs = nixpkgs.legacyPackages."x86_64-linux";
+    in {
+      devShells."x86_64-linux".default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          cargo rustc rust-analyzer clippy rustfmt rustlings
+        ];
+        env.RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+
+      shellHook = ''
+        # cd rustlings
+        # zellij --layout layout.kdl
+        '';
+      };
+
+
+  };
+}
